bf6c3836faf1c054c9816f43d471410cc42b0d79
Venugopal311 <venugopalglb@gmail.com>
2025-01-02 22:30:37 +0530

    New project

---
+++ src/main/java/com/project/spring/monthlyexpensestracker/item/ItemControllerJpa.java
@@ -0,0 +1,140 @@
+package com.project.spring.monthlyexpensestracker.item;
+
+import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Controller
+@SessionAttributes({"budget","name"})
+public class ItemControllerJpa {
+
+    private ItemList itemList;
+    private ItemListJpa itemListJpa;
+
+
+    private String localDate=LocalDate.now().toString();
+    public ItemControllerJpa(ItemList itemList,ItemListJpa itemListJpa){
+        this.itemList=itemList;
+        this.itemListJpa=itemListJpa;
+    }
+
+
+    @RequestMapping("/goto-expense")
+    public String showExpense(ModelMap model){
+       String username= getLoggedInUsername(model);
+        List<Item> items= itemListJpa.findByUsername(username);
+        model.addAttribute("items",items);
+        model.put("localDate",localDate);
+        model.put("total",itemList.getTotal(username));
+
+        model.putIfAbsent("budget", 0.0);
+
+        double safeToSpend= (Double) model.get("budget");
+
+        double toSpend = itemList.getSafeToSpend(username, safeToSpend);
+        if(toSpend<0)
+            model.put("lessThan","You have exhausted your savings ):");
+        model.put("safeToSpend", toSpend);
+        return "expense";
+    }
+
+
+
+    @RequestMapping(value="/add-expense",method = RequestMethod.GET)
+    public String showAddExpensePage(ModelMap model){
+        String username= getLoggedInUsername(model);
+        Item item=new Item(username,0,"",0,LocalDate.now(),1);
+
+        model.put("item",item);
+        return "expense-add";
+    }
+
+
+//    @RequestMapping(value="/add-expense",method = RequestMethod.POST)
+//    public String returnToExpense(@RequestParam String name,@RequestParam double price){
+//         itemList.addItem(name,price);
+//        return "redirect:/goto-expense";
+//    }
+
+    //changes done here
+    @RequestMapping(value="/add-expense",method = RequestMethod.POST)
+    public String addExpense(ModelMap model,@Valid Item item,BindingResult result) {
+        String userName= getLoggedInUsername(model);
+
+        if(result.hasErrors() || item.getPrice()<0){
+            return "expense-add";
+        }
+
+     itemList.addItem(userName, item.getName(),item.getPrice(),item.getLocalDate(),item.getQuantity());
+
+        return "redirect:/goto-expense";
+    }
+
+
+    @RequestMapping(value="/delete-item",method = RequestMethod.GET)
+    public String deleteItem(@RequestParam int serialNo,ModelMap model){
+       String username= getLoggedInUsername(model);
+      itemList.deleteBySerialNo(username,serialNo);
+        return "redirect:/goto-expense";
+    }
+
+
+    @RequestMapping(value="/update-item",method=RequestMethod.GET)
+    public String showUpdatePage(@RequestParam int serialNo,ModelMap model){
+        Item item=itemList.findBySerialNo(serialNo);
+        model.put("item",item);
+        return "expense-add";
+    }
+
+
+    @RequestMapping(value="/update-item",method=RequestMethod.POST)
+    public String updateTOdo(@Valid Item item,BindingResult result){
+
+        if(result.hasErrors())
+            return "expense-add";
+        itemList.updateItem(item);
+        return "redirect:/goto-expense";
+    }
+
+
+
+    private static String getLoggedInUsername(ModelMap model) {
+         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return    authentication.getName();
+    }
+
+    @RequestMapping(value = "filter-by-date",method = RequestMethod.GET)
+    public String filterDate(ModelMap model){
+        String username= getLoggedInUsername(model);
+        Item item=new Item(username,0,"",0,LocalDate.now(),0);
+
+        model.put("item",item);
+        return "filterByDate";
+    }
+
+    @RequestMapping(value = "filter-by-date",method = RequestMethod.POST)
+    public String ShowFilterByDatePage(@RequestParam LocalDate localDate,ModelMap model) {
+        List<Item> items=itemList.filterByDate(localDate, getLoggedInUsername(model));
+        model.addAttribute("items",items);
+        model.put("total",itemList.getTotal(items));
+        if(items.isEmpty()){
+            model.put("errorMessage","No items are available with date "+localDate);
+        }
+        return "expenseFilterByDate";
+
+    }
+
+
+
+}
